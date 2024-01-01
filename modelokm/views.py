d92819ea46c1a5a1074a35da429290a36e775284
Daniel Gongora <danielgongora6@hotmail.com>
2023-12-31 19:16:26 -0500

    start with django for backend

---
+++ modelokm/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+import cv2
+import matplotlib.pyplot as plt
+from django.http import HttpResponse
+
+# Create your views here.
+def processing(request):
+    imagen = np.array(cv2.imread(r'C:\Users\danie\OneDrive\Escritorio\djangoproject\modelokm\prueba.jpg'))
+    #:Hacerle esto al resultad0-->img = cv2.cvtColor(imagen , cv2.COLOR_BGR2RGB) #Busca usar RGB
+    # Obtiene las dimensiones y
+    # Vectoriza la imagen
+    a,l,c=tuple(imagen.shape)
+    pix=np.reshape(imagen,(a*l,c))
+    return HttpResponse(pix)
